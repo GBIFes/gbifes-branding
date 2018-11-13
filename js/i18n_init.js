91296741a55c3bd6fac2155fcb977f680113d006
GBIF development <support@gbif.es>
2018-11-13 13:13:40 +0100

    Added missing js/i18n_init.js

--- js/i18n_init.js
+++ js/i18n_init.js
@@ -3,33 +3,33 @@
  */
 
 
-const enabledLangs = ['es', 'en', 'ca'];
+var enabledLangs = ["es", "en", "ca"];
 
-let locale;
+var locale;
 
 function i18n_init() {
-  const url = new URL(window.location.href);
+    var url = new URL(window.location.href);
+    
+    locale = url.searchParams.get("lang");
 
-  locale = url.searchParams.get('lang');
+    if (locale != null){
+	localStorage.setItem("locale", locale);
+    } else {
+	locale = localStorage.getItem("locale");
+    }
 
-  if (locale != null) {
-    localStorage.setItem('locale', locale);
-  } else {
-    locale = localStorage.getItem('locale');
-  }
+    if (locale === undefined || locale === null){
+	locale = navigator.language.substring(0,2);
+    }
 
-  if (locale === undefined || locale === null) {
-    locale = navigator.language.substring(0, 2);
-  }
+    if (gbifesjs.isDevel) console.log(`Initial locale: ${locale}`);
+    
 
-  if (gbifesjs.isDevel) console.log(`Initial locale: ${locale}`);
-
-
-  const isValid = (enabledLangs.indexOf(locale) > -1);
-  if (!isValid) {
-    locale = 'es';
-    localStorage.setItem('locale', locale);
-  }
+    var isValid = (enabledLangs.indexOf(locale) > -1);
+    if (!isValid) {
+	locale = "es";
+	localStorage.setItem("locale", locale);	
+    }
 }
 
 i18n_init();
