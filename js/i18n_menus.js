619568e1814515b1e54ebcfc9f6260cd34754ceb
GBIF development <support@gbif.es>
2018-11-13 12:47:18 +0100

    Some fixes in i18n with #hashes

--- js/i18n_menus.js
+++ js/i18n_menus.js
@@ -2,33 +2,48 @@
  * Simple function to internationalise menus and sections of the home page.
  */
 function i18n_menus(){
-    var url = new URL(window.location.href);
-    locale = url.searchParams.get("lang");
-    if (locale != null){
-	localStorage.setItem("locale", locale);
-    } else {
-	locale = localStorage.getItem("locale");
-    }
+    if (gbifesjs.isDevel) console.log(`locale in i18n_menus: ${locale}`);
 
-    if (locale === undefined || locale === null){
-	locale = navigator.language.substring(0,2);
-    }
-
-    if (gbifesjs.isDevel) console.log(`Initial locale: ${locale}`);
-    
-    var enabledLangs = ["es", "en", "ca"];
-    var isValid = (enabledLangs.indexOf(locale) > -1);
-    if (!isValid) {
-	locale = "es";
-	localStorage.setItem("locale", locale);	
-    }
-
-    if (locale){
+    // Add lang param to lang links
+    if (locale) {
 	$(".top-nav-menu a, .portal-link").each(function() {
-	    var $this = $(this);       
-	    var _href = $this.attr("href"); 
-	    $this.attr("href", _href + '?lang=' + locale);
-	});	
+	    var $this = $(this);
+	    var _href = $this.attr("href");
+	    // Take care of #hash in links
+	    var uri = _href.split("#")[0];
+	    var hash = _href.split("#")[1];
+	    if (typeof hash === 'undefined') {
+		// without hash
+		$this.attr("href", _href + '?lang=' + locale);
+	    } else {
+		$this.attr("href", uri + '?lang=' + locale + "#" + hash);
+	    }
+	    if (gbifesjs.isDevel) console.log(`Added lang to href: ${$this.attr("href")}`);
+	});
+    }
+
+    var url = window.location.href;
+    
+    for (var i = 0; i < enabledLangs.length; i++) {
+	var curlang = enabledLangs[i];
+	var $link = $("." + curlang + '-locale-link'); // or grab it by tagname etc
+
+	var localeUrl = url;
+
+	if (url.includes('lang=')) {
+            localeUrl = url.replace(/lang=en|lang=es|lang=ca/gi, "lang=" + curlang);
+	} else {
+            if (url.includes('?')) {
+		localeUrl = localeUrl + "&lang=" + curlang;
+            } else {
+		var uri = url.split("#")[0];
+		var hash = url.split("#")[1];
+		hash = typeof hash === 'undefined'? "" : "#" + hash;
+		localeUrl = uri + "?lang=" + curlang + hash;
+            }
+	}
+	$link.attr('href', localeUrl);
+	if (gbifesjs.isDevel) console.log(`Added lang to href: ${$link.attr("href")}`);
     }
 
     // underscore current locale
@@ -42,64 +57,63 @@ function i18n_menus(){
 
     var path=`${gbifesjs.layoutUrl}i18n/`;
     
-    if (gbifesjs.isDevel) console.log(`locale: ${locale}`);
     if (gbifesjs.isDevel) console.log(`localePath: ${path}`);
 
-	jQuery.i18n.properties({
-	    name:'messages', 
-	    path: path,
-	    mode:'both',
-	    language: locale,
-	    async: true,
-	    callback: function() {
-		var keys = [
-		    'main_title_label',
-		    'menu_portal_part1',
-		    'menu_portal_part2',
-		    'menu_collections',
-		    'menu_datasets',
-		    'menu_search',
-		    'menu_explore',
-		    'menu_regions',
-		    'top_menu_dataportal',
-		    'footer_menu_about',
-		    'footer_menu_biodiversity_data',
-		    'footer_menu_collaborations',
-		    'footer_menu_resources',
-		    'footer_menu_news',
-		    'footer_menu_training',
-		    'footer_menu_software',
-		    'footer_menu_contact',
-		    'numbers_occurrences_label',
-		    'numbers_datasets_label',
-		    'numbers_institutions_label',
-		    'lang_link_en',
-		    'lang_link_es',
-		    'lang_link_cat',
-		    'footer_legal_info',
-		    'search_input_advanced1',
-		    'search_input_advanced2',
-		    'sub_menu_collections',
-		    'sub_menu_collections_detail',
-		    'sub_menu_datasets',
-		    'sub_menu_datasets_detail',
-		    'sub_menu_explore',
-		    'sub_menu_explore_detail',
-		    'sub_menu_regions',
-		    'sub_menu_regions_detail',
-		    'footer_legal_code',
-		    'banner_search_input_placeholder',
-		    'main_search_input_placeholder'
-		];
+    jQuery.i18n.properties({
+	name:'messages', 
+	path: path,
+	mode:'both',
+	language: locale,
+	async: true,
+	callback: function() {
+	    var keys = [
+		'main_title_label',
+		'menu_portal_part1',
+		'menu_portal_part2',
+		'menu_collections',
+		'menu_datasets',
+		'menu_search',
+		'menu_explore',
+		'menu_regions',
+		'top_menu_dataportal',
+		'footer_menu_about',
+		'footer_menu_biodiversity_data',
+		'footer_menu_collaborations',
+		'footer_menu_resources',
+		'footer_menu_news',
+		'footer_menu_training',
+		'footer_menu_software',
+		'footer_menu_contact',
+		'numbers_occurrences_label',
+		'numbers_datasets_label',
+		'numbers_institutions_label',
+		'lang_link_en',
+		'lang_link_es',
+		'lang_link_cat',
+		'footer_legal_info',
+		'search_input_advanced1',
+		'search_input_advanced2',
+		'sub_menu_collections',
+		'sub_menu_collections_detail',
+		'sub_menu_datasets',
+		'sub_menu_datasets_detail',
+		'sub_menu_explore',
+		'sub_menu_explore_detail',
+		'sub_menu_regions',
+		'sub_menu_regions_detail',
+		'footer_legal_code',
+		'banner_search_input_placeholder',
+		'main_search_input_placeholder'
+	    ];
 
-		for (var i=0; i< keys.length; i++){
-		    if(keys[i].endsWith('_placeholder')){
-			var elementID = keys[i].substring(0, keys[i].length - 12);
-			$('#' + elementID).attr('placeholder', jQuery.i18n.prop(keys[i]));
-		    } else {
-			$('#' + keys[i]).html(jQuery.i18n.prop(keys[i]));	
-		    }		
-		}
+	    for (var i=0; i< keys.length; i++){
+		if(keys[i].endsWith('_placeholder')){
+		    var elementID = keys[i].substring(0, keys[i].length - 12);
+		    $('#' + elementID).attr('placeholder', jQuery.i18n.prop(keys[i]));
+		} else {
+		    $('#' + keys[i]).html(jQuery.i18n.prop(keys[i]));	
+		}		
 	    }
-	});
-    }
+	}
+    });
+}
